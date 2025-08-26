2976e79e73fa433ae5427d94c28bd6ea728de3c0
Aditya Roy <138622344+AdityaOO73@users.noreply.github.com>
2025-08-26 21:10:16 +0530

    Delete models directory

--- models/Subscriber.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const subscriberSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  createdAt: { type: Date, default: Date.now },
-});
-
-export default mongoose.model("Subscriber", subscriberSchema);
