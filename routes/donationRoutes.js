4f3b1f2f3a9b31e923b3e7919a08346f3a2b73cb
Aditya Roy <138622344+AdityaOO73@users.noreply.github.com>
2025-08-26 21:10:04 +0530

    Delete routes directory

--- routes/donationRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { donateToCampaign } from "../controllers/donationController.js";
-
-const router = express.Router();
-router.post("/", donateToCampaign);
-
-export default router;
--- routes/subscribeRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { subscribeEmail } from "../controllers/subscribeController.js";
-
-const router = express.Router();
-router.post("/", subscribeEmail);
-
-export default router;
