30bd48e40b0a8117422285718f787b105ea3f110
Aditya Roy <138622344+AdityaOO73@users.noreply.github.com>
2025-08-26 21:07:30 +0530

    Delete controllers directory

--- controllers/contactController.js
+++
@@ -1,7 +0,0 @@
-import Contact from "../models/Contact.js";
-
-export const saveContact = async (req, res) => {
-  const contact = new Contact(req.body);
-  await contact.save();
-  res.json({ success: true, message: "Message saved successfully" });
-};
